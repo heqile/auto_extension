a517c210ac073c580b590cad1a7d67086611de84
LELE <mr.qile@gmail.com>
2020-11-05 20:59:46 +0100

    load data from storage

--- js/options.js
+++ js/options.js
@@ -1,4 +1,5 @@
 var jsonViewer = new JSONViewer();
 document.querySelector("#json").appendChild(jsonViewer.getContainer());
-var json = {"test":"toto"};
-jsonViewer.showJSON(json);
+chrome.storage.local.get("data", function(login_data) {
+    jsonViewer.showJSON(login_data.data);
+});
