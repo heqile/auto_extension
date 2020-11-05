07185f84532d22fa6ce09ce90a04710e07fda7fe
Qile HE <mr.qile@gmail.com>
2020-11-05 17:15:52 +0100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,16 +1,14 @@
-var login_data = {}
 
-function init() {
-    loadConfigFromFile();
-};
-
-function loadConfigFromFile() {
+function refreshConfigFromFile() {
     fetch(chrome.runtime.getURL("config/data.json"))
     .then(response => response.json())
     .then(data => {
-        login_data = data;
+        chrome.storage.local.set({"data": data}, function() {
+            console.log("save config");
+        });
     });
 };
+
 
 function checkUrl(tabId, changeInfo, tab) {
     // trick to get url's domain
@@ -18,38 +16,40 @@ function checkUrl(tabId, changeInfo, tab) {
     url.href = tab.url;
 
     if (changeInfo.status === "complete") {
-        for (const domain in login_data) {
-            if (url.hostname === domain) {
-                // inject code
-                chrome.tabs.insertCSS(tabId, {
-                    file: 'lib/css/bootstrap.min.css'
-                });
-                chrome.tabs.executeScript(tabId, {
-                    file: 'lib/js/jquery-3.5.1.slim.min.js'
-                });
-                chrome.tabs.executeScript(tabId, {
-                    file: 'lib/js/bootstrap.bundle.min.js'
-                });
-                // nested executeScript to pass config to content script
-                chrome.tabs.executeScript(tabId, {
-                    code: "var config = " + JSON.stringify(login_data[domain])
-                }, function() {
-                    chrome.tabs.executeScript(tabId, {
-                        file: 'js/content.js'
+        chrome.storage.local.get("data", function(login_data) {
+            for (const domain in login_data.data) {
+                if (url.hostname === domain) {
+                    // inject code
+                    chrome.tabs.insertCSS(tabId, {
+                        file: 'lib/css/bootstrap.min.css'
                     });
-                });
+                    chrome.tabs.executeScript(tabId, {
+                        file: 'lib/js/jquery-3.5.1.slim.min.js'
+                    });
+                    chrome.tabs.executeScript(tabId, {
+                        file: 'lib/js/bootstrap.bundle.min.js'
+                    });
+                    // nested executeScript to pass config to content script
+                    chrome.tabs.executeScript(tabId, {
+                        code: "var config = " + JSON.stringify(login_data.data[domain])
+                    }, function() {
+                        chrome.tabs.executeScript(tabId, {
+                            file: 'js/content.js'
+                        });
+                    });
+                }
             }
-        }
+        });
     }
 };
 
 chrome.tabs.onUpdated.addListener(checkUrl);
 
 // init when window loaded
-chrome.windows.onCreated.addListener(init);
+chrome.windows.onCreated.addListener(refreshConfigFromFile);
 
 // init when click on icon
-chrome.browserAction.onClicked.addListener(init);
+chrome.browserAction.onClicked.addListener(refreshConfigFromFile);
 
 // init when extension installed
-chrome.runtime.onInstalled.addListener(init);
+chrome.runtime.onInstalled.addListener(refreshConfigFromFile);
